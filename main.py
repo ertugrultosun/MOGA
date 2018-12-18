0d854c275a9bca1b3d5d2576d96f3ec7aebe319d
Ertugrul Tosun <mail@ertugrultosun.com>
2018-12-18 17:32:15 +0300

    preparation for iteration function

--- main.py
+++ main.py
@@ -101,11 +101,6 @@ def printparent(parent):
             print(parent[i][j]) 
 
 def is_pareto(costs, maximise=False):
-    """
-    :param costs: An (n_points, n_costs) array
-    :maximise: boolean. True for maximising, False for minimising
-    :return: A (n_points, ) boolean array, indicating whether each point is Pareto efficient
-    """
     is_efficient = np.ones(costs.shape[0], dtype = bool)
     for i, c in enumerate(costs):
         if is_efficient[i]:
@@ -114,6 +109,9 @@ def is_pareto(costs, maximise=False):
             else:
                 is_efficient[is_efficient] = np.any(costs[is_efficient]<=c, axis=1)  # Remove dominated points
     return is_efficient
+
+def iteration():
+    return True
 
 parse("data.txt")
 createpopulation(population)
